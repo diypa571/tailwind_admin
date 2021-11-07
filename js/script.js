a4a1dd2cf52a6f2a1153200d1b8eda241106a12c
Diyp <diypa571@gmail.com>
2021-11-07 23:43:04 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function init() {
+navFunction();
+}
+
+
+function navFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+window.addEventListener("load",init);
